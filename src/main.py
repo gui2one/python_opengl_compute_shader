b68d078ad82c4fd0d60d4f23acd5ea37383eaef1
gui2one <guillaume.rouault.fx@gmail.com>
2025-03-01 12:51:26 +0100

    added pyimgui to the project

--- src/main.py
+++ src/main.py
@@ -2,11 +2,29 @@ import glfw
 import os
 from OpenGL import GL
 from Shader import Shader, ShaderSources
+import imgui
+from imgui.integrations.glfw import GlfwRenderer
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__)) 
 SHADERS_DIR = os.path.join(ROOT_DIR, "shaders")
 shader : Shader = None
+IMGUI_IMPL = None
 
+def init_imgui():
+    # initilize imgui context (see documentation)
+    imgui.create_context()
+    
+    imgui.get_io().display_size = 500, 500
+    imgui.get_io().fonts.get_tex_data_as_rgba32()
+    
+def imgui_start_frame():
+    imgui.new_frame()
+    
+def imgui_end_frame():
+    # pass all drawing comands to the rendering pipeline
+    # and close frame context
+    imgui.render()
+    imgui.end_frame()    
 def load_shader_source(path : str):
     with open(path, "r") as f:
         return f.read()
@@ -34,19 +52,32 @@ def init_window(width, height, title):
     sources = ShaderSources()
     sources.compute_source = load_shader_source(f"{SHADERS_DIR}/simple_compute.glsl")
     shader = Shader(sources=sources)
+    
+
     return window
 
 def render_loop(window):
+
     while not glfw.window_should_close(window):
+        glfw.poll_events()
+        IMGUI_IMPL.process_inputs()
         GL.glClear(GL.GL_COLOR_BUFFER_BIT)
         GL.glClearColor(0.1, 0.2, 0.3, 1.0)  # Dark blueish background
-
-        # shader.use()
+        imgui_start_frame()
+        imgui.begin("First window", True)
+        imgui.text("hello")
+        imgui.end()
+        imgui_end_frame()
+        
+        IMGUI_IMPL.render(imgui.get_draw_data())
         glfw.swap_buffers(window)
-        glfw.poll_events()
+
 
 def main():
+    global IMGUI_IMPL
     window = init_window(800, 600, "OpenGL Compute Shader Example")
+    init_imgui()
+    IMGUI_IMPL = GlfwRenderer(window)
     render_loop(window)
     glfw.terminate()
 
